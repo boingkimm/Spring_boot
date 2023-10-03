f921afb945ba3a159deb034f1a9ff676c90c992b
Kim Bohee <boingkimm@gmail.com>
2023-10-03 22:27:20 +0900

    bean

---
+++ boot_01_빈생성/src/main/java/com/exam/dao/DeptDAO.java
@@ -0,0 +1,13 @@
+package com.exam.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DeptDAO {
+
+	public DeptDAO() {
+		System.out.println("DeptDAO 생성");
+	}
+
+	
+}
---
+++ boot_01_빈생성/src/main/java/com/exam/service/DeptServiceImpl.java
@@ -0,0 +1,13 @@
+package com.exam.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptServiceImpl {
+
+	public DeptServiceImpl() {
+		System.out.println("DeptServiceImpl 생성");
+	}
+
+	
+}
